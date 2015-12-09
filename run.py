6bc31ac4233a2aca64380564673299a631546919
Kazuya <www.kazuya.kawakami@gmail.com>
2015-12-09 15:33:43 -0500

    add lab output

--- run.py
+++ run.py
@@ -20,8 +20,9 @@ def char2rgb(function, color2index, word):
             err = True 
 
     pred = function([chars_encoded])
+    lab  = map(float, pred) 
     rgb  = lab2rgb(pred)
-    return rgb, err
+    return rgb, lab, err
 
 
 
@@ -31,11 +32,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Give Me Conll data.')
     parser.add_argument('-f', type=str, help='provide text file')
     parser.add_argument('-ht', type=int, default=0, help='specify ourput wormat. text: 0, html: 1')
-    parser.add_argument('-c',  type=int, default=0, help='if you want to run character by character set 1')
+    parser.add_argument('-l',  type=int, default=0, help='to get lab result, set 1')
+    parser.add_argument('-c',  type=int, default=0, help='to run character by character set 1')
     args = parser.parse_args()
 
     if not args.ht:
         print 'word, R, G, B, OOV'
+
+    if args.l == 1:
+        assert args.ht == 0, 'Lab output is anly available for text output, set -ht = 0'
 
     for line in open(args.f):
         word = line.strip()
@@ -47,12 +52,15 @@ if __name__ == '__main__':
             words = [word]
 
         for word in words:
-            [r,g,b], err  = char2rgb(function, color2index, word) 
+            rgb, lab, err  = char2rgb(function, color2index, word) 
 
             if args.ht:
                 if err:
-                    print "<span><span style='font-weight: bold; color:rgb({},{},{})'>{}</span> (There were out-of-character)</span>".format(r, g, b, word) 
+                    print "<span><span style='font-weight: bold; color:rgb({},{},{})'>{}</span> (There were out-of-character)</span>".format(rgb[0], rgb[1], rgb[2], word) 
                 else:
-                    print "<span style='font-weight: bold; color:rgb({},{},{})'>{} </span>".format(r, g, b, word) 
+                    print "<span style='font-weight: bold; color:rgb({},{},{})'>{} </span>".format(rgb[0], rgb[1], rgb[2], word) 
             else:
-                print "{},{},{},{},{}".format(word, r, g, b, err) 
+                if args.l:
+                    print "{},{},{},{},{}".format(word, lab[0], lab[1], lab[2], err) 
+                else:
+                    print "{},{},{},{},{}".format(word, rgb[0], rgb[1], rgb[2], err) 
